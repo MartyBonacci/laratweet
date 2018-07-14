122f170fd3f74ca9a79d56796688974b20b741d5
Marty Bonacci <martybonacci@gmail.com>
2018-07-14 17:39:43 -0600

    timeline controller return home view

--- app/Http/Controllers/TimelineController.php
+++ app/Http/Controllers/TimelineController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
 class TimelineController extends Controller
 {
-    //
+    public function index() {
+        return view('home');
+    }
 }
