ed0fa084ca0c396145475617adfdf65b7e920355
Marty Bonacci <martybonacci@gmail.com>
2018-07-15 08:13:57 -0600

    added user id and body to post migration

--- database/migrations/2018_07_15_140221_create_posts_table.php
+++ database/migrations/2018_07_15_140221_create_posts_table.php
@@ -15,7 +15,11 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('body',140);
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
