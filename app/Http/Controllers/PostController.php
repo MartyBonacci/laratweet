401bf4d33c2d6e5acff613ab83037bfa424c0cad
Marty Bonacci <martybonacci@gmail.com>
2018-07-16 20:00:25 -0600

    return post with user id

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller {
-    public function create(Request $request) {
+    public function create(Request $request, Post $post) {
         // create post
-        $request->user()->posts()->create([
+        $createdPost = $request->user()->posts()->create([
             'body' => $request->body,
         ]);
         // return the response
-
+        return response()->json($post->with('user')->find($createdPost->id));
     }
 }
