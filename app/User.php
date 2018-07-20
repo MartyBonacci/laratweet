e822add613f3854e611faca3103da5842f58db9b
Marty Bonacci <martybonacci@gmail.com>
2018-07-19 22:27:27 -0600

    created UserController Route and get by username

--- app/User.php
+++ app/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable
     public function getAvatarAttribute() {
         return $this->getAvatar();
     }
+
+    public function getRouteKeyName() {
+        return 'username';
+    }
 }
