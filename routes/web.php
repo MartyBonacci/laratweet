c8c442b981d160fc80f92d74f3e0352b94431ae3
Marty Bonacci <martybonacci@gmail.com>
2018-07-22 18:47:43 -0600

    show list of followers and links to them

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/', 'TimelineController@index');
     Route::post('/posts','PostController@create');
-    Route::get('/users/{user}','UserController@index');
+    Route::get('/users/{user}','UserController@index')->name('users');
     Route::get('/users/{user}/follow','UserController@follow')->name('users.follow');
     Route::get('/users/{user}/unfollow','UserController@unfollow')->name('users.unfollow');
 });
